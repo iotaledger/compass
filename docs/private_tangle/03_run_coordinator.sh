6b7f9058d35d3f91d6cddcd71debe62808a94552
Andreas C. Osowski <andreas@osowski.de>
2018-10-18 16:47:51 +0200

    docs/private_tangle: add missing security parameter to coordinator runargs

--- docs/private_tangle/03_run_coordinator.sh
+++ docs/private_tangle/03_run_coordinator.sh
@@ -8,6 +8,7 @@ docker run -t --rm -v `pwd`/data:/data iota/compass/docker:coordinator coordinat
 	-sigMode $sigMode \
 	-powMode $powMode \
 	-mwm $mwm \
+	-security $security \
 	-seed $seed \
 	-tick $tick \
 	-host $host \
