5ceb1f69f27fa0d055b04b1a2f6eb37f5c506495
Andreas C. Osowski <andreas@osowski.de>
2018-10-24 17:55:51 +0200

    docs/private_tangle: adjust scripts for new LayersCalculator

--- docs/private_tangle/01_generate_addresses.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-. lib.sh
-load_config
-
-docker run -t --rm -v `pwd`/data:/data iota/compass/docker:address_generator address_generator_deploy.jar $sigMode $seed $security $depth /data/addresses.csv
