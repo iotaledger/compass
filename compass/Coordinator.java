a2d84c4e18eb4f6fe649edb5b550e578ea5495bd
Andrea Villa <andreakarimodm@gmail.com>
2019-03-17 15:36:04 +0530

    Print state exception messages in logs too

--- compass/Coordinator.java
+++ compass/Coordinator.java
@@ -116,7 +116,7 @@ public class Coordinator {
       } catch (Exception e) {
         String msg = "Error loading Compass state file '" + statePath + "'! State file required if not bootstrapping.";
 
-        log.error(msg);
+        log.error(msg, e.getMessage());
         throw new RuntimeException(e);
       }
     }
@@ -336,7 +336,7 @@ public class Coordinator {
       } catch (Exception e) {
         String msg = "Error saving Compass state to file '" + statePath + "'!";
 
-        log.error(msg);
+        log.error(msg, e.getMessage());
         throw new RuntimeException(e);
       }
 
