64590a03be6de7e6d8eaa08a008195684d2a7243
Alon Elmaliah <alon.elmaliah@gmail.com>
2019-04-03 17:13:18 +0300

    kill containers only if they are running

--- CI/cleanUp.sh
+++ CI/cleanUp.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
 echo "cleaning up"
-docker kill $(docker ps | grep coordinator | cut -f1 -d\ )
-docker kill $(docker ps | grep iri | cut -f1 -d\ )
+COO_CONTAINER=`docker ps | grep coordinator | cut -f1 -d\ `
+if [ "$COO_CONTAINER" ]; then
+    docker kill $COO_CONTAINER
+fi
+
+IRI_CONTAINER=`docker ps | grep iri | cut -f1 -d\ `
+if [ "$IRI_CONTAINER" ]; then
+    docker kill $IRI_CONTAINER
+fi
+
 SIG_SOURCE_SERVER_CONTAINER=`docker ps | grep signature_source_server | cut -f1 -d\ `
 if [ "$SIG_SOURCE_SERVER_CONTAINER" ]; then
-    docker kill $(docker ps | grep signature_source_server | cut -f1 -d\ )
+    docker kill $SIG_SOURCE_SERVER_CONTAINER
 fi
 
 rm -rf docs/private_tangle/data
